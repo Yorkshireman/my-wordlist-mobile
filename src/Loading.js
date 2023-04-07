2e8e5c28018d3fc925b18189130a27fed6e1334c
Yorkshireman <>
2023-04-07 14:38:21 +0100

    add code comment

--- src/Loading.js
+++ src/Loading.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './styles';
 import { ActivityIndicator, View } from 'react-native';
 
+// TODO: replace with react-native-paper components
 export const Loading = () => (
   <View style={styles.centered}>
     <ActivityIndicator color='red' size="large" />
