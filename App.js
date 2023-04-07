dcbba90e2a47f9a0dfd9183c50cf2c8b97e827fd
Yorkshireman <>
2023-04-08 00:30:16 +0100

    fetch wordlist data

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { customColours as colors } from './utils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { customColours as colors } from './src/utils';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/HomeScreen';
 import { LogInScreen } from './src/LogInScreen';
@@ -8,17 +9,29 @@ import NetworkLogger from 'react-native-network-logger';
 import { SignUpScreen } from './src/SignUpScreen';
 import { startNetworkLogging } from 'react-native-network-logger';
 import { StatusBar } from 'expo-status-bar';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, ApolloProvider, concat, HttpLink, InMemoryCache } from '@apollo/client';
 import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { MY_WORDLIST_GRAPHQL_URL, NETWORK_LOGGER } from '@env';
 
+
+const authMiddleware = new ApolloLink(async (operation, forward) => {
+  const authToken = await AsyncStorage.getItem('myWordlistAuthToken');
+
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: authToken
+    }
+  }));
+
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({ uri: MY_WORDLIST_GRAPHQL_URL });
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  headers: {
-    // this is the user_id from the rails app's seed data
-    authorization: 'b0f9ad83-9148-4a6e-98bc-ac9216e645e7'
-  },
-  uri: MY_WORDLIST_GRAPHQL_URL
+  link: concat(authMiddleware, httpLink)
 });
 
 const networkLoggerIsEnabled = NETWORK_LOGGER === 'true';
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,13 +1,27 @@
+import { MY_WORDLIST } from './graphql-queries';
 import PropTypes from 'prop-types';
 import sharedStyles from './styles';
 import { Text } from 'react-native';
 import { useAuthToken } from './hooks';
+import { useEffect } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import { View } from 'react-native';
 
 export const HomeScreen = ({ navigation }) => {
   const authToken = useAuthToken(navigation);
+  const [getWordlist, { data, error, loading }] = useLazyQuery(MY_WORDLIST);
 
   console.log('authToken: ', authToken);
+
+  useEffect(() => {
+    if (!authToken) {
+      return;
+    }
+    console.log('getWordlist');
+    getWordlist();
+  }, [getWordlist, authToken]);
+
+  console.log(JSON.stringify(data, null, 2));
 
   return (
     <View style={{ ...sharedStyles.container, alignItems: 'center' }}>
