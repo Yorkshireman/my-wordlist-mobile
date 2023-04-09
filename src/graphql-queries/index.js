a4199ac9a848a54c29660736ab1aee21836ce980
Yorkshireman <>
2023-04-10 00:38:35 +0100

    correct import

--- src/graphql-queries/index.js
+++ src/graphql-queries/index.js
@@ -1,4 +1,5 @@
-import { MY_WORDLIST, MY_WORDLIST_CREATE } from './myWordlist';
+import { MY_WORDLIST } from './myWordlist';
+import { MY_WORDLIST_CREATE } from './myWordlistCreate';
 
 export {
   MY_WORDLIST,
