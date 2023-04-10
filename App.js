f3eac6a844cb6e8707db9bd45123ff0ecf17225b
Yorkshireman <>
2023-04-10 15:57:32 +0100

    Remove errorLink Causes network requests to fail on my android device. Failed to work out why.

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationBar } from './src/components';
 import { NavigationContainer } from '@react-navigation/native';
 import NetworkLogger from 'react-native-network-logger';
-import { onError } from '@apollo/client/link/error';
 import { startNetworkLogging } from 'react-native-network-logger';
 import { StatusBar } from 'expo-status-bar';
-import { ApolloClient, ApolloLink, ApolloProvider, from, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, ApolloProvider, concat, HttpLink, InMemoryCache } from '@apollo/client';
 import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { HomeScreen, LogInScreen, SignUpScreen } from './src/screens';
 import { MY_WORDLIST_GRAPHQL_URL, NETWORK_LOGGER } from '@env';
@@ -26,22 +25,11 @@ const authMiddleware = new ApolloLink(async (operation, forward) => {
   return forward(operation);
 });
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
-
 const httpLink = new HttpLink({ uri: MY_WORDLIST_GRAPHQL_URL });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: from([authMiddleware, errorLink, httpLink])
+  link: concat(authMiddleware, httpLink)
 });
 
 const networkLoggerIsEnabled = NETWORK_LOGGER === 'true';
