de48cdfc483558fb10f33af6f7168ead251939c7
Yorkshireman <>
2023-04-29 21:37:35 +0100

    use wordlist entry fragment in mutation queries too

--- src/graphql-queries/wordlistEntryUpdate.js
+++ src/graphql-queries/wordlistEntryUpdate.js
@@ -1,16 +1,14 @@
 import { gql } from '@apollo/client';
+import { WORDLIST_ENTRY } from '../fragments';
 
 export const WORDLIST_ENTRY_UPDATE = gql`
   mutation UpdateWordlistEntry($id: ID!, $wordlistEntryInput: WordlistEntryInput!) {
     authToken
     wordlistEntryUpdate(id: $id, wordlistEntryInput: $wordlistEntryInput) {
       wordlistEntry {
-        id
-        categories {
-          id
-          name
-        }
+        ...WordlistEntryFields
       }
     }
   }
+  ${WORDLIST_ENTRY}
 `;
