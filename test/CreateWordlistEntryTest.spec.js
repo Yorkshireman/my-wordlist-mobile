bc67e5f58f96ac2c1648d6a868d8eb5c19c776ac
Yorkshireman <>
2023-05-13 20:40:25 +0100

    test seems to be working

--- test/CreateWordlistEntryTest.spec.js
+++ test/CreateWordlistEntryTest.spec.js
@@ -1,27 +1,36 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../src/screens';
 import { MockedProvider } from '@apollo/client/testing';
-import { render } from '@testing-library/react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { useLazyQuery } from '@apollo/client';
+import { render, waitFor } from '@testing-library/react-native';
 
-// jest.mock('@apollo/client', () => ({
-//   ...jest.requireActual(),
-//   useLazyQuery: jest.fn()
-// }));
+jest.mock('@apollo/client', () => ({
+  ...jest.requireActual('@apollo/client'),
+  useLazyQuery: jest.fn()
+}));
 
 jest.mock('@react-native-async-storage/async-storage', () => ({ getItem: jest.fn() }));
 
-test('Can create a wordlist entry', () => {
-  AsyncStorage.getItem.mockResolvedValue('mockAuthTokenValue');
-  const navigation = { navigate: jest.fn() };
-  useLazyQuery.mockReturnValue({ data: {} });
-  console.log('-----------', useLazyQuery());
+describe('Feature: Adding a word', () => {
+  test('can add a word', async () => {
+    AsyncStorage.getItem.mockResolvedValue('mockAuthTokenValue');
+    useLazyQuery.mockReturnValue([ jest.fn(), { data: {}}]);
+    const Stack = createNativeStackNavigator();
 
-  render(
-    <MockedProvider addTypename={false}>
-      <HomeScreen navigation={navigation} />
-    </MockedProvider>
-  );
+    await waitFor(() => {
+      render(
+        <MockedProvider addTypename={false}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen component={HomeScreen} name="Home" options={{ title: 'My Wordlist' }} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </MockedProvider>
+      );
+    });
 
-  expect(AsyncStorage.getItem).toHaveBeenCalledWith('myWordlistAuthToken');
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith('myWordlistAuthToken');
+  });
 });
