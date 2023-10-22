ad0cc935b0f6be5a5952f22a3a07f0b1eaee2270
Yorkshireman <>
2023-10-22 23:02:39 +0100

    add button to expand form

--- src/components/create-wordlist-entries/CreateWordlistEntriesForm.js
+++ src/components/create-wordlist-entries/CreateWordlistEntriesForm.js
@@ -1,6 +1,6 @@
-import { Button } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import { WordlistEntry } from './WordlistEntry';
+import { Button, IconButton } from 'react-native-paper';
 import { useAsyncStorage, useWordlistEntriesCreate } from '../../hooks';
 import { useEffect, useState } from 'react';
 
@@ -15,14 +15,17 @@ const defaultWordlistEntries = [
 
 export const CreateWordlistEntriesForm = ({ setModalVisible, wordlistId }) => {
   const currentAuthToken = useAsyncStorage();
+  const [addWordlistEntryButtonIsDisabled, setAddWordlistEntryButtonIsDisabled] = useState(true);
   const [submitButtonIsDisabled, setSubmitButtonIsDisabled] = useState(true);
   const [wordlistEntries, setWordlistEntries] = useState(defaultWordlistEntries);
   const wordlistEntriesCreate = useWordlistEntriesCreate({ currentAuthToken, wordlistEntries, wordlistId });
 
   useEffect(() => {
-    if (wordlistEntries.find(wordlistEntry => wordlistEntry?.word?.text?.length)) {
+    if (wordlistEntries.every(wordlistEntry => wordlistEntry?.word?.text?.length)) {
+      setAddWordlistEntryButtonIsDisabled(false);
       setSubmitButtonIsDisabled(false);
     } else {
+      setAddWordlistEntryButtonIsDisabled(true);
       setSubmitButtonIsDisabled(true);
     }
   }, [setSubmitButtonIsDisabled, wordlistEntries]);
@@ -43,12 +46,18 @@ export const CreateWordlistEntriesForm = ({ setModalVisible, wordlistId }) => {
       {wordlistEntries.map(({ word }, i) =>
         <WordlistEntry index={i} key={i} setWordlistEntries={setWordlistEntries} word={word} />
       )}
+      <IconButton
+        disabled={addWordlistEntryButtonIsDisabled}
+        icon="camera"
+        onPress={() => setWordlistEntries([...wordlistEntries, { categories: [], word: { text: '' }}])}
+        size={20}
+      />
       <Button
         contentStyle={{ flexDirection: 'row-reverse' }}
+        disabled={submitButtonIsDisabled}
         icon='send'
         mode='contained'
         onPress={onSubmit}
-        submitButtonIsDisabled={submitButtonIsDisabled}
       >
         Add
       </Button>
