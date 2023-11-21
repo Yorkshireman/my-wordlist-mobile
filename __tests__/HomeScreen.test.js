06c492b5a9c7de0e4bbac7b3308ea580f465a90d
Yorkshireman <>
2023-11-21 16:57:41 +0000

    move mock into own file

--- __tests__/HomeScreen.test.js
+++ __tests__/HomeScreen.test.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { HomeScreen } from '../src/screens';
 import { MockedProvider } from '@apollo/client/testing';
-import { MY_WORDLIST } from '../src/graphql-queries';
+import { myWordlistQueryMock } from '../mockedProviderMocks/my-wordlist';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react-native';
@@ -10,49 +10,6 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react-nativ
 jest.useFakeTimers();
 
 const mockNavigation = { navigate: jest.fn() };
-const mocks = [
-  {
-    request: {
-      query: MY_WORDLIST
-    },
-    result: {
-      data: {
-        authToken: 'auth-token-from-query-response',
-        myWordlist: {
-          __typename: 'MyWordlist',
-          entries: [
-            {
-              __typename: 'WordlistEntry',
-              categories: [
-                {
-                  __typename: 'Category',
-                  id: '905651d6-2d66-44c3-9e89-7ef076afb6b5',
-                  name: 'noun'
-                },
-                {
-                  __typename: 'Category',
-                  id: 'f7302234-57b4-4234-b9c7-5483a84e6bf7',
-                  name: 'tech'
-                }
-              ],
-              createdAt: '2023-11-20T02:52:30Z',
-              id: 'ac6adf87-5b0c-433f-8efc-7e090c030aef',
-              word: {
-                __typename: 'Word',
-                createdAt: '2023-10-29T19:11:14Z',
-                id: '4ecf7f86-c394-4a66-8152-1100af8a6cc7',
-                text: 'phone'
-              },
-              wordId: '4ecf7f86-c394-4a66-8152-1100af8a6cc7',
-              wordlistId: 'de728808-3df2-4dfc-adf9-5981ee5f795a'
-            }
-          ],
-          id: 'de728808-3df2-4dfc-adf9-5981ee5f795a'
-        }
-      }
-    }
-  }
-];
 
 describe('HomeScreen', () => {
   afterEach(() => jest.clearAllMocks());
@@ -61,7 +18,7 @@ describe('HomeScreen', () => {
     beforeEach(() => {
       render(
         <NavigationContainer>
-          <MockedProvider addTypename={true} mocks={mocks}>
+          <MockedProvider addTypename={true} mocks={myWordlistQueryMock}>
             <HomeScreen navigation={mockNavigation} />
           </MockedProvider>
         </NavigationContainer>
@@ -82,7 +39,8 @@ describe('HomeScreen', () => {
   });
 
   describe('when auth token is in storage', () => {
-    const { categories } = mocks[0].result.data.myWordlist.entries[0];
+    const { categories } = myWordlistQueryMock[0].result.data.myWordlist.entries[0];
+
     beforeEach(async () => {
       AsyncStorage.getItem.mockImplementation((key) => {
         if (key === 'myWordlistAuthToken') {
@@ -96,7 +54,7 @@ describe('HomeScreen', () => {
         render(
           <PaperProvider>
             <NavigationContainer>
-              <MockedProvider addTypename={true} mocks={mocks}>
+              <MockedProvider addTypename={true} mocks={myWordlistQueryMock}>
                 <HomeScreen navigation={mockNavigation} />
               </MockedProvider>
             </NavigationContainer>
@@ -128,7 +86,7 @@ describe('HomeScreen', () => {
         render(
           <PaperProvider>
             <NavigationContainer>
-              <MockedProvider addTypename={true} mocks={mocks}>
+              <MockedProvider addTypename={true} mocks={myWordlistQueryMock}>
                 <HomeScreen navigation={mockNavigation} />
               </MockedProvider>
             </NavigationContainer>
---
+++ mockedProviderMocks/my-wordlist.js
@@ -0,0 +1,45 @@
+import { MY_WORDLIST } from '../src/graphql-queries';
+
+export const myWordlistQueryMock = [
+  {
+    request: {
+      query: MY_WORDLIST
+    },
+    result: {
+      data: {
+        authToken: 'auth-token-from-query-response',
+        myWordlist: {
+          __typename: 'MyWordlist',
+          entries: [
+            {
+              __typename: 'WordlistEntry',
+              categories: [
+                {
+                  __typename: 'Category',
+                  id: '905651d6-2d66-44c3-9e89-7ef076afb6b5',
+                  name: 'noun'
+                },
+                {
+                  __typename: 'Category',
+                  id: 'f7302234-57b4-4234-b9c7-5483a84e6bf7',
+                  name: 'tech'
+                }
+              ],
+              createdAt: '2023-11-20T02:52:30Z',
+              id: 'ac6adf87-5b0c-433f-8efc-7e090c030aef',
+              word: {
+                __typename: 'Word',
+                createdAt: '2023-10-29T19:11:14Z',
+                id: '4ecf7f86-c394-4a66-8152-1100af8a6cc7',
+                text: 'phone'
+              },
+              wordId: '4ecf7f86-c394-4a66-8152-1100af8a6cc7',
+              wordlistId: 'de728808-3df2-4dfc-adf9-5981ee5f795a'
+            }
+          ],
+          id: 'de728808-3df2-4dfc-adf9-5981ee5f795a'
+        }
+      }
+    }
+  }
+];
