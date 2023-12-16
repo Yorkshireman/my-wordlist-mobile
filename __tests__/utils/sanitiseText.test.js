f31afe0ce33f0b48cdacfa8d339c340328f032c9
Yorkshireman <>
2023-12-16 17:26:49 +0000

    add null guards to sanitiseText()

--- __tests__/utils/sanitiseText.test.js
+++ __tests__/utils/sanitiseText.test.js
@@ -6,4 +6,16 @@ describe('sanitiseText()', () => {
     const actualValue = sanitiseText('TeXt  ');
     expect(actualValue).toBe(expectedValue);
   });
+
+  test('returns an empty string when passed an empty string', () => {
+    expect(sanitiseText('')).toBe('');
+  });
+
+  test('throws error with appropriate message when passed undefined', () => {
+    expect(() => sanitiseText()).toThrow('sanitiseText(): str parameter is undefined.');
+  });
+
+  test('throws error with appropriate message when passed null', () => {
+    expect(() => sanitiseText(null)).toThrow('sanitiseText(): str parameter is null.');
+  });
 });
--- src/utils/sanitiseText.js
+++ src/utils/sanitiseText.js
@@ -1,3 +1,11 @@
 export const sanitiseText = str => {
+  if (str === undefined) {
+    throw new Error('sanitiseText(): str parameter is undefined.');
+  }
+
+  if (str === null) {
+    throw new Error('sanitiseText(): str parameter is null.');
+  }
+
   return str.trim().toLowerCase();
 };
