f4b74cd4f4bc1d3c2fa2faef9af73f437d79b758
Yorkshireman <>
2023-12-16 17:05:12 +0000

    use sanitiseText() in parseCategories()

--- src/utils/parseCategories.js
+++ src/utils/parseCategories.js
@@ -1,4 +1,6 @@
+import { sanitiseText } from './sanitiseText';
+
 export const parseCategories = str => {
   if (!str) throw new Error('str param cannot be falsey');
-  return str.split(',').map(str => ({ name: str.trim().toLowerCase() }));
+  return str.split(',').map(str => ({ name: sanitiseText(str) }));
 };
