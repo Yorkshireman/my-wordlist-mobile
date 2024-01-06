9a655cc1165be781ad7e0c3ce2a992046b959db2
Yorkshireman <>
2024-01-06 17:07:53 +0000

    remove redundant describe block

--- __tests__/screens/LogInScreen.test.js
+++ __tests__/screens/LogInScreen.test.js
@@ -9,39 +9,37 @@ jest.useFakeTimers();
 const mockNavigation = { navigate: jest.fn() };
 
 describe('LogInScreen', () => {
-  describe('when submitting empty input fields', () => {
-    beforeEach(() => {
-      render(
-        <NavigationContainer>
-          <MockedProvider>
-            <LogInScreen navigation={mockNavigation} />
-          </MockedProvider>
-        </NavigationContainer>
-      );
-    });
+  beforeEach(() => {
+    render(
+      <NavigationContainer>
+        <MockedProvider>
+          <LogInScreen navigation={mockNavigation} />
+        </MockedProvider>
+      </NavigationContainer>
+    );
+  });
 
-    test('displays email input field', async () => {
-      await waitFor(() => expect(screen.getByTestId('login-email-input-field')).toBeOnTheScreen());
-    });
+  test('displays email input field', async () => {
+    await waitFor(() => expect(screen.getByTestId('login-email-input-field')).toBeOnTheScreen());
+  });
 
-    test('displays email input placeholder text', async () => {
-      await waitFor(() => expect(screen.getByPlaceholderText('email')).toBeOnTheScreen());
-    });
+  test('displays email input placeholder text', async () => {
+    await waitFor(() => expect(screen.getByPlaceholderText('email')).toBeOnTheScreen());
+  });
 
-    test('displays password input field', async () => {
-      await waitFor(() => expect(screen.getByTestId('login-password-input-field')).toBeOnTheScreen());
-    });
+  test('displays password input field', async () => {
+    await waitFor(() => expect(screen.getByTestId('login-password-input-field')).toBeOnTheScreen());
+  });
 
-    test('displays password input placeholder text', async () => {
-      await waitFor(() => expect(screen.getByPlaceholderText('password')).toBeOnTheScreen());
-    });
+  test('displays password input placeholder text', async () => {
+    await waitFor(() => expect(screen.getByPlaceholderText('password')).toBeOnTheScreen());
+  });
 
-    test('displays submit button', async () => {
-      await waitFor(() => expect(screen.getByRole('button', { name: 'Log in' })).toBeOnTheScreen());
-    });
+  test('displays submit button', async () => {
+    await waitFor(() => expect(screen.getByRole('button', { name: 'Log in' })).toBeOnTheScreen());
+  });
 
-    test('displays navigate to sign up text', async () => {
-      await waitFor(() => expect(screen.getByRole('button', { name: 'New user? Sign up' })).toBeOnTheScreen());
-    });
+  test('displays navigate to sign up text', async () => {
+    await waitFor(() => expect(screen.getByRole('button', { name: 'New user? Sign up' })).toBeOnTheScreen());
   });
 });
