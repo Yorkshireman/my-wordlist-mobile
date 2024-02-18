fc1e44f38465dc48466a2a353cdb9d16eee830e0
Yorkshireman <>
2024-02-18 22:19:52 +0000

    apply gradient to rhs of ScrollView component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,3 +1,4 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { PropTypes } from 'prop-types';
 import { Chip, useTheme } from 'react-native-paper';
 import { ScrollView, StyleSheet } from 'react-native';
@@ -6,20 +7,28 @@ export const Categories = ({ categories }) => {
   const { colors } = useTheme();
 
   return (
-    <ScrollView horizontal>
-      {categories.map(({ id, name}) => {
-        return (
-          <Chip
-            compact
-            key={id}
-            style={{ ...styles.category, backgroundColor: colors.primary }}
-            textStyle={{ color: 'white' }}
-          >
-            {name}
-          </Chip>
-        );
-      })}
-    </ScrollView>
+    <>
+      <ScrollView horizontal>
+        {categories.map(({ id, name}) => {
+          return (
+            <Chip
+              compact
+              key={id}
+              style={{ ...styles.category, backgroundColor: colors.primary }}
+              textStyle={{ color: 'white' }}
+            >
+              {name}
+            </Chip>
+          );
+        })}
+      </ScrollView>
+      <LinearGradient
+        colors={['transparent', 'rgb(242, 242, 242)']}
+        end={{ x: 1, y: 0 }}
+        start={{ x: 0.7, y: 0 }}
+        style={styles.gradient}
+      />
+    </>
   );
 };
 
@@ -32,5 +41,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     height: 32,
     marginRight: 1
+  },
+  gradient: {
+    height: '100%',
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    width: 25
   }
 });
