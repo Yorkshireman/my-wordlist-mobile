0bb626a54233cd29b1adfff58f6032c7169f8372
Yorkshireman <>
2024-05-07 00:51:40 +0100

    mock drawer to get tests to pass

--- jest-setup.js
+++ jest-setup.js
@@ -15,3 +15,11 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('expo-linear-gradient', () => ({
   LinearGradient: 'LinearGradient'
 }));
+
+// Couldn't get the recommended approach working: https://reactnavigation.org/docs/testing#mocking-native-modules
+jest.mock('react-native-drawer-layout', () => {
+  const View = require('react-native').View;
+  return {
+    Drawer: View
+  };
+});
