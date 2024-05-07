c2c20f6bcf832062ec469b179d788fe4cb781b8d
Yorkshireman <>
2024-05-07 17:31:26 +0100

    write test for opening filters drawer

---
+++ src/components/Filters.js
@@ -0,0 +1,6 @@
+import { Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+
+export const Filters = () => {
+  return <View><Text>Hello</Text></View>;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { CreateWordlistEntryForm } from './CreateWordlistEntryForm';
 export { EditWordForm } from './EditWordForm';
 export { Error } from './Error';
 export { EyeIcon } from './EyeIcon';
+export { Filters } from './Filters';
 export { Loading } from './Loading';
 export { NavigationBar } from './NavigationBar';
 export { NotificationProvider } from './NotificationProvider';
