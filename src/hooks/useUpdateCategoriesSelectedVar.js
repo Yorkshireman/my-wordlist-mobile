9f60e7059d5ce480190ec4b2c47b5accf7bd87f7
Yorkshireman <>
2024-05-12 19:30:21 +0100

    add code comment

--- src/hooks/useUpdateCategoriesSelectedVar.js
+++ src/hooks/useUpdateCategoriesSelectedVar.js
@@ -2,6 +2,8 @@ import { categoriesSelectedVar } from '../reactiveVars';
 import { useEffect } from 'react';
 import { useReactiveVar } from '@apollo/client';
 
+// if wordlist categories have changed, rendering any selected categories redundant, remove them
+// from the categoriesSelectedVar
 export const useUpdateCategoriesSelectedVar = wordlistCategories => {
   const categoriesSelected = useReactiveVar(categoriesSelectedVar);
 
