fb0f6cf272139d1af1d86a6ef0e45faea13e4275
Yorkshireman <>
2024-05-12 01:35:32 +0100

    rename useAuthToken hook

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import { Drawer } from 'react-native-drawer-layout';
 import PropTypes from 'prop-types';
 import sharedStyles from '../styles';
-import { useAuthToken } from '../hooks';
+import { useFetchWordlistData } from '../hooks';
 import { useState } from 'react';
 import { FAB, IconButton } from 'react-native-paper';
 import { Filters, Loading, Wordlist } from '../components';
 import { StyleSheet, View } from 'react-native';
 
 export const HomeScreen = ({ navigation }) => {
-  const { data: { myWordlist } = {}, loading } = useAuthToken(navigation);
+  const { data: { myWordlist } = {}, loading } = useFetchWordlistData(navigation);
   const [open, setOpen] = useState(false);
 
   return (
