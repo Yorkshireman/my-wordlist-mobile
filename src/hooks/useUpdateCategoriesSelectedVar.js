6cf8093335380300fe63b63d09bf98746b4d3ffa
Yorkshireman <>
2024-06-09 15:52:41 +0100

    fix test and rename var

--- src/hooks/useUpdateCategoriesSelectedVar.js
+++ src/hooks/useUpdateCategoriesSelectedVar.js
@@ -2,8 +2,8 @@ import { categoriesSelectedVar } from '../reactiveVars';
 import { useEffect } from 'react';
 import { useReactiveVar } from '@apollo/client';
 
-// if wordlist categories have changed, rendering any selected categories redundant, remove them
-// from the categoriesSelectedVar
+// if wordlist categories have changed in such a way that renders any selected categories redundant,
+// remove them from the categoriesSelectedVar
 export const useUpdateCategoriesSelectedVar = wordlistCategories => {
   const categoriesSelected = useReactiveVar(categoriesSelectedVar);
 
@@ -12,9 +12,9 @@ export const useUpdateCategoriesSelectedVar = wordlistCategories => {
       return wordlistCategories.some(({ id }) => id === categoryId);
     };
 
-    const filteredCategoriesSelected = categoriesSelected.filter(isIncludedCategoryInWordlist);
-    if (filteredCategoriesSelected.sort().toString() !== categoriesSelected.sort().toString()) {
-      categoriesSelectedVar(filteredCategoriesSelected);
+    const nonOrphanedSelectedCategories = categoriesSelected.filter(isIncludedCategoryInWordlist);
+    if (nonOrphanedSelectedCategories.sort().toString() !== categoriesSelected.sort().toString()) {
+      categoriesSelectedVar(nonOrphanedSelectedCategories);
     }
   }, [categoriesSelected, wordlistCategories]);
 };
