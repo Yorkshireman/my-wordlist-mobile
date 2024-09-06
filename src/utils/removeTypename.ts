2e3137ab4da2fbfc4e5be8a3372ecb68449ef103
Yorkshireman <>
2024-09-06 22:08:17 +0100

    remove any from omitTypename()

--- src/utils/removeTypename.ts
+++ src/utils/removeTypename.ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const omitTypename = (key: string, value: any) => {
+const omitTypename = (key: string, value: unknown) => {
   return key === '__typename' ? undefined : value;
 };
 
-export const removeTypename = (operationName: string, variables: Record<string, any>) => {
+export const removeTypename = (operationName: string, variables: Record<string, unknown>) => {
   if (!variables)
     throw new Error(`removeTypename(), variables arg is falsey. Operation name: ${operationName}`);
 
   return JSON.parse(JSON.stringify(variables, omitTypename));
 };
-/* eslint-enable @typescript-eslint/no-explicit-any */
