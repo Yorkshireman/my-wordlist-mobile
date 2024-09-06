58ede4013d5110ae86a74cac1d91970e035bbd11
Yorkshireman <>
2024-09-06 21:29:21 +0100

    Refactor: Create WordlistEntry component

--- src/components/Wordlist.js
+++ src/components/Wordlist.js
@@ -1,10 +1,9 @@
 import { calculateLongestWordLength } from '../utils';
-import { Categories } from './Categories';
 import { DeleteConfirm } from './DeleteConfirm';
-import { useNavigation } from '@react-navigation/native';
-import { IconButton, Menu, Text, useTheme } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+import { Text } from 'react-native-paper';
+import { WordlistEntry } from './WordlistEntry';
 import { MY_WORDLIST, WORDLIST_ENTRY_DELETE } from '../graphql-queries';
-import { ScrollView, StyleSheet, View } from 'react-native';
 import { useAsyncStorage, useFilters } from '../hooks';
 import { useMemo, useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
@@ -16,12 +15,7 @@ export const Wordlist = () => {
     anyFiltersApplied,
     myWordlist: { entries }
   } = useFilters(data);
-  const navigation = useNavigation();
-  const {
-    colors: { secondaryContainer }
-  } = useTheme();
   const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
-  const [wordlistEntryMenuVisible, setWordlistEntryMenuVisible] = useState(null);
   const [wordlistEntryIdToDelete, setWordlistEntryIdToDelete] = useState();
   const [wordlistEntryDelete] = useMutation(WORDLIST_ENTRY_DELETE, {
     optimisticResponse: () => ({
@@ -66,58 +60,19 @@ export const Wordlist = () => {
           You might want to adjust your filters :-)
         </Text>
       ) : (
-        entries.map(({ categories, id, word: { text } }) => {
-          return (
-            <View
-              aria-label='wordlist-entry'
-              key={id}
-              style={{ ...styles.entry, borderBottomColor: secondaryContainer }}
-              testID={id}
-            >
-              <View style={{ ...styles.word, flexBasis: wordFlexBasis }}>
-                <Text variant={'bodyLarge'}>{text}</Text>
-              </View>
-              <View style={styles.addCategories.wrapper}>
-                <Categories categories={categories} />
-              </View>
-              <View style={{ justifyContent: 'center', marginLeft: 'auto' }}>
-                <Menu
-                  anchor={
-                    <IconButton
-                      icon='dots-vertical'
-                      onPress={() => {
-                        setWordlistEntryMenuVisible(id);
-                      }}
-                      size={24}
-                      style={{ margin: 0 }}
-                      testID={`wordlist-entry-menu-${id}`}
-                    />
-                  }
-                  onDismiss={() => setWordlistEntryMenuVisible(null)}
-                  visible={wordlistEntryMenuVisible === id}
-                >
-                  <Menu.Item
-                    leadingIcon='note-edit-outline'
-                    onPress={() => {
-                      setWordlistEntryMenuVisible(null);
-                      navigation.navigate('EditWordlistEntry', { id });
-                    }}
-                    title='Edit'
-                  />
-                  <Menu.Item
-                    leadingIcon='trash-can-outline'
-                    onPress={() => {
-                      setWordlistEntryIdToDelete(id);
-                      setWordlistEntryMenuVisible(null);
-                      setShowDeleteConfirm(true);
-                    }}
-                    title='Delete'
-                  />
-                </Menu>
-              </View>
-            </View>
-          );
-        })
+        entries.map(({ categories, id, word: { text } }) => (
+          <WordlistEntry
+            {...{
+              categories,
+              id,
+              setShowDeleteConfirm,
+              setWordlistEntryIdToDelete,
+              text,
+              wordFlexBasis
+            }}
+            key={id}
+          />
+        ))
       )}
       <DeleteConfirm
         confirm={() => {
@@ -133,32 +88,3 @@ export const Wordlist = () => {
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  addCategories: {
-    icon: {
-      alignItems: 'flex-end',
-      height: '100%',
-      margin: 0,
-      marginRight: 3,
-      width: 20
-    },
-    wrapper: {
-      columnGap: 2,
-      flex: 1,
-      flexDirection: 'row'
-    }
-  },
-  entry: {
-    borderBottomWidth: 1,
-    columnGap: 5,
-    flexDirection: 'row',
-    paddingBottom: 2,
-    paddingTop: 2
-  },
-  word: {
-    flexBasis: 100,
-    justifyContent: 'center',
-    maxWidth: 185
-  }
-});
---
+++ src/components/WordlistEntry.js
@@ -0,0 +1,112 @@
+import { Categories } from './Categories';
+import PropTypes from 'prop-types';
+import { StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
+import { IconButton, Menu, Text, useTheme } from 'react-native-paper';
+import React, { useState } from 'react';
+
+export const WordlistEntry = ({
+  categories,
+  id,
+  setShowDeleteConfirm,
+  setWordlistEntryIdToDelete,
+  text,
+  wordFlexBasis
+}) => {
+  const navigation = useNavigation();
+  const {
+    colors: { secondaryContainer }
+  } = useTheme();
+
+  const [wordlistEntryMenuVisible, setWordlistEntryMenuVisible] = useState(null);
+
+  return (
+    <View
+      aria-label='wordlist-entry'
+      key={id}
+      style={{ ...styles.entry, borderBottomColor: secondaryContainer }}
+      testID={id}
+    >
+      <View style={{ ...styles.word, flexBasis: wordFlexBasis }}>
+        <Text variant={'bodyLarge'}>{text}</Text>
+      </View>
+      <View style={styles.addCategories.wrapper}>
+        <Categories categories={categories} />
+      </View>
+      <View style={{ justifyContent: 'center', marginLeft: 'auto' }}>
+        <Menu
+          anchor={
+            <IconButton
+              icon='dots-vertical'
+              onPress={() => {
+                setWordlistEntryMenuVisible(id);
+              }}
+              size={24}
+              style={{ margin: 0 }}
+              testID={`wordlist-entry-menu-${id}`}
+            />
+          }
+          onDismiss={() => setWordlistEntryMenuVisible(null)}
+          visible={wordlistEntryMenuVisible === id}
+        >
+          <Menu.Item
+            leadingIcon='note-edit-outline'
+            onPress={() => {
+              setWordlistEntryMenuVisible(null);
+              navigation.navigate('EditWordlistEntry', { id });
+            }}
+            title='Edit'
+          />
+          <Menu.Item
+            leadingIcon='trash-can-outline'
+            onPress={() => {
+              setWordlistEntryIdToDelete(id);
+              setWordlistEntryMenuVisible(null);
+              setShowDeleteConfirm(true);
+            }}
+            title='Delete'
+          />
+        </Menu>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  addCategories: {
+    icon: {
+      alignItems: 'flex-end',
+      height: '100%',
+      margin: 0,
+      marginRight: 3,
+      width: 20
+    },
+    wrapper: {
+      columnGap: 2,
+      flex: 1,
+      flexDirection: 'row'
+    }
+  },
+  entry: {
+    borderBottomWidth: 1,
+    columnGap: 5,
+    flexDirection: 'row',
+    paddingBottom: 2,
+    paddingTop: 2
+  },
+  word: {
+    flexBasis: 100,
+    justifyContent: 'center',
+    maxWidth: 185
+  }
+});
+
+WordlistEntry.propTypes = {
+  categories: PropTypes.array.isRequired,
+  id: PropTypes.string.isRequired,
+  setShowDeleteConfirm: PropTypes.func.isRequired,
+  setWordlistEntryIdToDelete: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  wordFlexBasis: PropTypes.number.isRequired
+};
