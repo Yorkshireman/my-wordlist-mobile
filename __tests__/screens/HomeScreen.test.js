3e1bb981e9044ef420cf7090b575519fbaf552d4
Yorkshireman <>
2025-04-19 18:23:03 +0100

    fix HomeScreen tests

--- __tests__/screens/HomeScreen.test.js
+++ __tests__/screens/HomeScreen.test.js
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../../src/screens';
 import { MockedProvider } from '@apollo/client/testing';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react-native';
-import { myWordlistQueryMock, myWordlistQueryMockWithOneEntry } from '../../mockedProviderMocks/my-wordlist';
+import {
+  myWordlistQueryMock,
+  myWordlistQueryMockWithOneEntry
+} from '../../mockedProviderMocks/my-wordlist';
 
 jest.useFakeTimers();
+
+const HomeScreenWrapper = navigation => {
+  const WrappedHomeScreen = () => <HomeScreen navigation={navigation} />;
+  return WrappedHomeScreen;
+};
 
 const mockNavigation = { navigate: jest.fn() };
 
@@ -15,21 +24,29 @@ describe('HomeScreen', () => {
   afterEach(() => jest.clearAllMocks());
 
   describe('when no auth token in storage', () => {
-    beforeEach(() => {
-      render(
-        <NavigationContainer>
-          <MockedProvider addTypename mocks={[myWordlistQueryMock]}>
-            <HomeScreen navigation={mockNavigation} />
-          </MockedProvider>
-        </NavigationContainer>
-      );
+    beforeEach(async () => {
+      const Stack = createNativeStackNavigator();
+      await waitFor(() => {
+        render(
+          <PaperProvider>
+            <NavigationContainer>
+              <MockedProvider addTypename mocks={[myWordlistQueryMock]}>
+                <Stack.Navigator>
+                  <Stack.Screen component={HomeScreenWrapper(mockNavigation)} name='Home' />
+                </Stack.Navigator>
+              </MockedProvider>
+            </NavigationContainer>
+          </PaperProvider>
+        );
+      });
     });
 
     test('calls navigate() once', () => {
       expect(mockNavigation.navigate).toHaveBeenCalledTimes(1);
     });
 
-    test('calls navigate() with \'LogIn\'', () => {
+    // eslint-disable-next-line quotes
+    test("calls navigate() with 'LogIn'", () => {
       expect(mockNavigation.navigate).toHaveBeenCalledWith('LogIn');
     });
 
@@ -50,11 +67,14 @@ describe('HomeScreen', () => {
       });
 
       await waitFor(() => {
+        const Stack = createNativeStackNavigator();
         render(
           <PaperProvider>
             <NavigationContainer>
               <MockedProvider addTypename={true} mocks={[myWordlistQueryMockWithOneEntry]}>
-                <HomeScreen navigation={mockNavigation} />
+                <Stack.Navigator>
+                  <Stack.Screen component={HomeScreenWrapper(mockNavigation)} name='Home' />
+                </Stack.Navigator>
               </MockedProvider>
             </NavigationContainer>
           </PaperProvider>
@@ -77,36 +97,22 @@ describe('HomeScreen', () => {
         expect(screen.getByText(name)).toBeOnTheScreen();
       });
     });
-  });
 
-  describe('Pressing FAB (+ sign) button', () => {
-    beforeEach(async () => {
-      await waitFor(() => {
-        render(
-          <PaperProvider>
-            <NavigationContainer>
-              <MockedProvider addTypename={true} mocks={[myWordlistQueryMock]}>
-                <HomeScreen navigation={mockNavigation} />
-              </MockedProvider>
-            </NavigationContainer>
-          </PaperProvider>
-        );
+    describe('pressing FAB (+ sign) button', () => {
+      beforeEach(async () => await waitFor(() => fireEvent.press(screen.getByTestId('fab'))));
+
+      test('calls navigate() once', async () => {
+        await waitFor(() => {
+          expect(mockNavigation.navigate).toHaveBeenCalledTimes(1);
+        });
       });
 
-      await waitFor(() => {
-        fireEvent.press(screen.getByTestId('fab'));
-      });
-    });
-
-    test('calls navigate() once', async () => {
-      await waitFor(() => {
-        expect(mockNavigation.navigate).toHaveBeenCalledTimes(1);
-      });
-    });
-
-    test('calls navigate() with CreateWordlistEntries', async () => {
-      await waitFor(() => {
-        expect(mockNavigation.navigate).toHaveBeenCalledWith('CreateWordlistEntries', { wordlistId: 'c722b3ef-c762-4245-a0fa-fb452c4539cf' });
+      test('calls navigate() with CreateWordlistEntries and expected wordlistId', async () => {
+        await waitFor(() => {
+          expect(mockNavigation.navigate).toHaveBeenCalledWith('CreateWordlistEntries', {
+            wordlistId: 'c722b3ef-c762-4245-a0fa-fb452c4539cf'
+          });
+        });
       });
     });
   });
